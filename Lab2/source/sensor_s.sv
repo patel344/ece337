8842fe2dc3369423e564ee73d2fb8abd8296d460
Parth Patel <patel344@purdue.edu>
2017-01-17 11:56:30 -0500

    Initial setup for Lab1

---
+++ Lab2/source/sensor_s.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   sensor_s.sv
+// Created:     1/17/2017
+// Author:      Parth Patel
+// Lab Section: 337-01
+// Version:     1.0  Initial Design Entry
+// Description: Structural Style Sensor Error Detector module.
