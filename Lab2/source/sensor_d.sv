3620bde8001f096fa1d2b0720672f431aeb4b0a6
Parth Patel <patel344@purdue.edu>
2017-01-17 13:51:44 -0500

    Adding Lab2 scripts

---
+++ Lab2/source/sensor_d.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   sensor_d.sv
+// Created:     1/17/2017
+// Author:      Parth Patel
+// Lab Section: 337-01
+// Version:     1.0  Initial Design Entry
+// Description: Dataflow Style Sensor Error Detector.
--- Lab2/source/sensor_s.sv
+++ Lab2/source/sensor_s.sv
@@ -5,3 +5,18 @@
 // Lab Section: 337-01
 // Version:     1.0  Initial Design Entry
 // Description: Structural Style Sensor Error Detector module.
+
+module sensor_s
+(
+	input wire [3:0] sensors,
+	output wire error
+);
+
+wire temp1;
+wire temp2;
+
+and A1 (temp1, sensors[1], sensors[3]);
+and A2 (temp2, sensors[1], sensors[2]);
+or A3 (error, temp1, temp2, sensors[0]);
+
+endmodule
